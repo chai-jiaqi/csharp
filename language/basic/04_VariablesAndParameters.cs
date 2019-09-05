374a6f9a13059ca373c139014dcc0fb0eb5b2f92
chaijiaqi <jiaqi.chai1@thoughtworks.com>
2019-09-05 12:06:51 +0800

    done part of language 04

--- language/basic/04_VariablesAndParameters.cs
+++ language/basic/04_VariablesAndParameters.cs
@@ -13,8 +13,8 @@ namespace basic
             var defaultValueDemo = new DefaultValueDemoClass();
 
             // change the variable values of the following 2 lines to correct values
-            var expectedReferenceTypeValue = new RefTypeClass(default(int));
-            const int expectedValueTypeValue = 1;
+            RefTypeClass expectedReferenceTypeValue = null;
+            const int expectedValueTypeValue = 0;
 
             Assert.Equal(expectedReferenceTypeValue, defaultValueDemo.referenceTypeValue);
             Assert.Equal(expectedValueTypeValue, defaultValueDemo.valueTypeValue);
@@ -26,10 +26,10 @@ namespace basic
         public void should_get_default_value_using_default_operator()
         {
             // change the variable values of the following 4 lines to correct values.
-            const int expectedDefaultIntResult = 1;
-            const bool expectedDefaultBoolResult = true;
-            const char expectedDefaultCharResult = 'a';
-            var expectedDefaultObjectResult = new object();
+            const int expectedDefaultIntResult = 0;
+            const bool expectedDefaultBoolResult = false;
+            const char expectedDefaultCharResult = '\0';
+            object expectedDefaultObjectResult = null;
 
             Assert.Equal(expectedDefaultIntResult, default(int));
             Assert.Equal(expectedDefaultBoolResult, default(bool));
@@ -45,7 +45,7 @@ namespace basic
             int passingInt = 1;
 
             // change the variable value to correct one.
-            const int expectedResult = 2;
+            const int expectedResult = 1;
 
             FunctionPassingIntAsArgument(passingInt);
 
@@ -59,7 +59,7 @@ namespace basic
             RefTypeClass modifiedRefTypeObject = FunctionPassingRefTypeClassAsArgument(refTypeObject);
 
             // change the variable value to correct one.
-            RefTypeClass expectedResult = modifiedRefTypeObject;
+            RefTypeClass expectedResult = refTypeObject;
 
             Assert.Same(expectedResult, refTypeObject);
         }
@@ -70,7 +70,7 @@ namespace basic
             int passingInt = 1;
 
             // change the variable value to correct one.
-            const int expectedResult = 1;
+            const int expectedResult = 2;
 
             FunctionPassingRefIntAsArgument(ref passingInt);
 
@@ -88,7 +88,7 @@ namespace basic
                 ref refTypeObject);
 
             // change the variable value to correct one
-            object expectedResult = refToOriginalObject;
+            object expectedResult = modifiedRefTypeObject;
 
             Assert.Same(expectedResult, refTypeObject);
         }
@@ -102,7 +102,7 @@ namespace basic
             FunctionPassingOutIntAsArgument(out passingInt);
 
             // change the variable value to correct one
-            const int expectedResult = default(int);
+            const int expectedResult = 2;
 
             Assert.Equal(expectedResult, passingInt);
         }
@@ -118,7 +118,7 @@ namespace basic
                 FunctionPassingOutRefTypeClassAsArgument(out refTypeObject);
             
             // change the variable value to correct one
-            object expectedResult = default(object);
+            object expectedResult = modifiedRefTypeObject;
 
             Assert.Equal(expectedResult, refTypeObject);
         }
@@ -129,7 +129,7 @@ namespace basic
             int sum = PassVariableLengthArguments(1, 2, 3, 4, 5);
 
             // change the variable value to correct one
-            const int expectedResult = default(int);
+            const int expectedResult = 15;
 
             Assert.Equal(expectedResult, sum);
         }
@@ -140,7 +140,7 @@ namespace basic
             int optionalParameterValue = PassAsOptionalArgument();
 
             // change the variable value to correct one
-            const int expectedResult = default(int);
+            const int expectedResult = 23;
 
             Assert.Equal(expectedResult, optionalParameterValue);
         }
