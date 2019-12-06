1dd0ef26bc7a0acd671de96056121af5926288a5
chaijiaqi <jiaqi.chai1@thoughtworks.com>
2019-12-06 20:16:10 +0800

    learned how the discrads works

--- language-edge/edge/03_Discards.cs
+++ language-edge/edge/03_Discards.cs
@@ -5,19 +5,23 @@ namespace edge
 {
     public class DiscardsFacts
     {
+        // The two _ is two thing, both of them is not in storage
+
         [Fact]
         [SuppressMessage("ReSharper", "ConvertToConstant.Local")]
         public void a_crazy_discard_test_you_will_like()
         {
+            // Only one _ in one scope 
             int _ = 0xABCD;
             int value = 0xCDEF;
-            if (int.TryParse("2345", out var _))
+            // _ used to discard the result
+            if (int.TryParse("2345", result: out var _))
             {
                 value = _;
             }
 
             // Please correct the following line to pass the test.
-            const int expected = default;
+            const int expected = 0xABCD;
             
             Assert.Equal(expected, value);
         }
